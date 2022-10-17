9ae51e523d1b26c5dc80a3664ad08b0f1883bfc6
Chibuike Akpulu <akpulukelvin@gmail.com>
2022-10-17 20:16:48 +0100

    added models and registered them to admin site

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+class Artiste(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.first_name
+
+    
+class Song(models.Model):
+    
+    title = models.CharField(max_length=50)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Lyric(models.Model):
+    
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+
--- songcrud/settings.py
+++ songcrud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1&n#!+qzocv6_c=k2p6pio^g#=8!84@^5e_w7kr9_c8l7jh6^e"
+SECRET_KEY = "safe-space"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
